b80a459059647c0cc1a13317198f7f62e2f26cb4
Serhii Kyslovets <hapless.spry_0k@icloud.com>
2023-04-06 15:57:02 +0300

    chore: added the soulution for the very first leetcode problem

--- src/two-sum.js
+++ src/two-sum.js
@@ -4,5 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-
+    for (let i = 0; i < nums.length - 1; i++) {
+        for(let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
 };
+
+console.log(twoSum([2,7,11,15], 9)); // O(n^2/2)
+
+
