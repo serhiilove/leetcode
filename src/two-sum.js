583b561ba94c698eb710ad2be5f6ea27a6fee9f4
Serhii Kyslovets <hapless.spry_0k@icloud.com>
2023-04-09 10:17:20 +0300

    chore: added better solution for the sum two problem

--- src/two-sum.js
+++ src/two-sum.js
@@ -3,6 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
+
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length - 1; i++) {
         for(let j = i + 1; j < nums.length; j++) {
@@ -13,6 +14,18 @@ var twoSum = function(nums, target) {
     }
 };
 
+const twoSum2 = function(nums, target) {
+    const ht = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const j = ht.get(target - nums[i]);
+        if (j !== undefined) {
+            return [j, i];
+        }
+
+        ht.set(nums[i], i);
+    }
+};
+
 console.log(twoSum([2,7,11,15], 9)); // O(n^2/2)
-
-
+console.log(twoSum2([2,7,11,15], 9)); // O(n)
