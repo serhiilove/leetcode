26a45c462a6f29d6c2c293838f1ad34d11af8788
Serhii Kyslovets <hapless.spry_0k@icloud.com>
2023-04-21 15:42:56 +0300

    feat: added solution for the merge-two-sorted-lists problem

---
+++ src/merge-two-sorted-lists.js
@@ -0,0 +1,48 @@
+const LinkedList = require('../data_structures/single-linked-list');
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+const mergeTwoLists = function(list1, list2) {
+    const ll = new LinkedList();
+
+    let curr1 = list1;
+    let curr2 = list2;
+
+    while (curr1.next || curr2.next) {
+        if (curr1.next && !curr2.next) {
+            ll.add(curr1.val);
+            curr1 = curr1.next;
+        } else if (!curr1.next && curr2.next) {
+            ll.add(curr2.val);
+            curr2 = curr2.next;
+        } else {
+            if (curr1.val <= curr2.val) {
+                ll.add(curr1.val);
+                curr1 = curr1.next;
+            } else {
+                ll.add(curr2.val);
+                curr2 = curr2.next;
+            }
+        }
+    }
+
+    ll.add(Math.min(curr1.val, curr2.val));
+    ll.add(Math.max(curr1.val, curr2.val));
+
+    return ll.printList();
+};
+
+const l1 = { val: 1, next: { val: 2, next: { val: 4, next: null }}}; // 1 -> 2 -> 4
+const l2 = { val: 1, next: { val: 3, next: { val: 4, next: null }}}; // 1 -> 3 => 4
+
+console.log(mergeTwoLists(l1, l2));
